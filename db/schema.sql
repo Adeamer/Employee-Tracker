b1f067cf10cd5980ed0365ed08e89909c648ef39
Elziekio <anthony.deamer@hotmail.com>
2021-08-30 13:32:28 +1000

    Initial commit

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employee_db;
+CREAT DATABASE employee_db;
+
+USE DATABASE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+    FOREIGN KEY (id)
+    REFERENCES employees (id)
+);
+
+SELECT * FROM employees;
+SELECT * FROM roles;
+SELECT * FROM department;
